2139304e935afdb2ef51844541ad8b7e0d51e8d6
Faruk-Ceker <farukceker.te@gmail.com>
2023-08-26 16:46:17 +0300

    Main Class olusturuldu.

---
+++ src/MainClass/MainClass.java
@@ -0,0 +1,14 @@
+package MainClass;
+
+import Utility.BaseDriver;
+import com.google.common.annotations.VisibleForTesting;
+
+public class MainClass extends BaseDriver {
+
+    @VisibleForTesting
+    public void Test(){
+
+
+        BekleVeKapat();
+    }
+}
