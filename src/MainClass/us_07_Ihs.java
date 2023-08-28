70fb26b7cb170118f8bea870914afb12f18e9b07
LENOVO V15 Gen2 <ihsanahmedusmail@gmail.com>
2023-08-29 00:52:23 +0300

    demowebshop us_07_Pozitif+negatif tamamlandı

--- src/MainClass/us_07_Ihs.java
+++ src/MainClass/us_07_Ihs.java
@@ -2,6 +2,7 @@ package MainClass;
 
 import Utility.BaseDriver;
 import Utility.MyFunc;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -14,6 +15,8 @@ public class us_07_Ihs extends BaseDriver {
 
         driver.get("https://demowebshop.tricentis.com/");
         MyFunc.Wait(2);
+
+        String anketTercih = "Excellent"; // Anket menu: Excellent, Good, Poor, Very bad
 
         WebElement login = driver.findElement(By.xpath("//a[contains(text(), 'Log in')]"));
         login.click();
@@ -31,7 +34,20 @@ public class us_07_Ihs extends BaseDriver {
         logInBtn.click();
         MyFunc.Wait(2);
 
+        WebElement communityPoll = driver.findElement(By.xpath("//input[@type='radio' and following-sibling::label[contains(text(), '"+anketTercih+"')]]"));
+        communityPoll.click();
+        MyFunc.Wait(2);
 
+        WebElement voteBtn = driver.findElement(By.id("vote-poll-1"));
+        voteBtn.click();
+        MyFunc.Wait(2);
+
+        WebElement anketSonuc = driver.findElement(By.xpath("//div[@id='poll-block-1']//ul[@class='poll-results']"));
+        String sonucMesaji = anketSonuc.getText();
+        System.out.println("sonucMesaji = " + sonucMesaji);
+        MyFunc.Wait(2);
+
+        Assert.assertTrue("Anket sonucu gözükmedi",sonucMesaji != null && !sonucMesaji.isEmpty());
 
         BekleVeKapat();
     }
@@ -42,6 +58,23 @@ public class us_07_Ihs extends BaseDriver {
 
         driver.get("https://demowebshop.tricentis.com/");
         MyFunc.Wait(2);
+
+        String anketTercih = "Excellent"; // Anket menu: Excellent, Good, Poor, Very bad
+
+        WebElement communityPoll = driver.findElement(By.xpath("//input[@type='radio' and following-sibling::label[contains(text(), '"+anketTercih+"')]]"));
+        communityPoll.click();
+        MyFunc.Wait(2);
+
+        WebElement voteBtn = driver.findElement(By.id("vote-poll-1"));
+        voteBtn.click();
+        MyFunc.Wait(2);
+
+        WebElement anketSonuc = driver.findElement(By.id("block-poll-vote-error-1"));
+        String sonucMesaji = anketSonuc.getText();
+        System.out.println("sonucMesaji = " + sonucMesaji);
+        MyFunc.Wait(2);
+
+        Assert.assertTrue("Login yapmadan anket doldurma testi başarısız", sonucMesaji.contains("Only registered users can vote"));
 
 
 
