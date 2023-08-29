229339c048bbb8eebcaddfdca3045d8d1640ce4a
Faruk-Ceker <farukceker.te@gmail.com>
2023-08-29 15:39:41 +0300

    US_04 Test Class oluşturuldu

---
+++ src/MainClass/US_04_Faruk.java
@@ -0,0 +1,25 @@
+package MainClass;
+
+import Utility.BaseDriver;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class US_04_Faruk extends BaseDriver {
+
+    @Test
+    public void Test_0401(){
+        driver.get("https://demowebshop.tricentis.com/]");
+
+        WebElement login = driver.findElement(By.xpath("//a[@class='ico-login']"));
+        login.click();
+        WebElement email = driver.findElement(By.id("Email"));
+        email.sendKeys("cek@example.com");
+        WebElement password = driver.findElement(By.id("Password"));
+        password.sendKeys("far123");
+        WebElement loginButton = driver.findElement(By.xpath("//input[@class='button-1 login-button']"));
+        loginButton.click();
+
+        BekleVeKapat();
+    }
+}
